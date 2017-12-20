a2d81eb1d2c2b1bc91d3a12769169cdd84e1956a
Maxime Chéramy <maxime@codingame.com>
2017-12-20 13:32:30 +0100

    update for game engine 1.5

--- src/main/resources/view/config.js
+++ src/main/resources/view/config.js
@@ -1,13 +1,5 @@
 import { GraphicEntityModule } from './entity-module/GraphicEntityModule.js';
 
-// You can define resources used by your games (like images, fonts, ...)
-export const assets = {
-  images: {
-    // ball: '/assets/ball.png',
-    // background: '/assets/background.jpg'
-  }
-};
-
 // List of viewer modules that you want to use in your game
 export const modules = [
 	GraphicEntityModule
